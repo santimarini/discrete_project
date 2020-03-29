e508dccd7bc41d524641f94187afa48f1be21013
Santi Marini <santiagomarini44@gmail.com>
2020-03-29 05:10:07 -0300

    extreme case: null graph

--- MARINI_Santiago__CENTINI_Joaquin/CangrejoEstelar/main.c
+++ MARINI_Santiago__CENTINI_Joaquin/CangrejoEstelar/main.c
@@ -4,21 +4,19 @@
 
 int main(int argc, char const *argv[])
 {
-	if (argc < 2){
-		fprintf(stderr, "few arguments: type ./a.out <filename>\n");
-		return -1;
-	}else if (argc > 2){
-		fprintf(stderr, "many arguments: type ./a.out <filename>\n");
-		return -1;
-	}
+    if (argc < 2){
+        fprintf(stderr, "few arguments: type ./a.out <filename>\n");
+        return -1;
+    }else if (argc > 2){
+        fprintf(stderr, "many arguments: type ./a.out <filename>\n");
+        return -1;
+    }
+    char const *filename = argv[1];
+    Grafo G;
+    G = ConstruccionDelGrafo(filename);
+    
+    if (G != NULL)
+        DestruccionDelGrafo(G);
 
-	char const *filename = argv[1];
-	Grafo G;
-	Grafo G2;
-	G = ConstruccionDelGrafo(filename);
-	G2 = CopiarGrafo(G);
-	DestruccionDelGrafo(G);
-	DestruccionDelGrafo(G2);
-
-	return 0;
+    return 0;
 }
