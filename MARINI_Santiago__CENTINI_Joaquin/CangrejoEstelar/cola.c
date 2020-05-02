004e76581de723759313e638a927eef189208800
Santi Marini <santiagomarini44@gmail.com>
2020-05-02 03:30:24 -0300

    queue struct and functions

--- MARINI_Santiago__CENTINI_Joaquin/CangrejoEstelar/cola.c
+++ MARINI_Santiago__CENTINI_Joaquin/CangrejoEstelar/cola.c
@@ -1,84 +1,80 @@
 #include <stdlib.h>
-#include "GrafoSt2020.h"
-#include "cola.h"
 #include "veinteveinte.h"
+#include "cola.h"
+#include <assert.h>
 
-ElementoC *CrearElementoC(u32 v,u32 i){
-    ElementoC *elemento = (ElementoC *)malloc(sizeof(ElementoC));
-    elemento->vertice = v;
-    elemento->indice = i;
-    elemento->siguiente = NULL;
-    return elemento;
+ElementoC *CrearElementoC(u32 v) {
+  ElementoC *elemento = (ElementoC *) malloc(sizeof(ElementoC));
+  elemento->vertice = v;
+  elemento->siguiente = NULL;
+  return elemento;
 }
 
-void DestruirElementoC(ElementoC *elemento){
+void DestruirElementoC(ElementoC *elemento) {
+  if(elemento != NULL) {
     elemento->siguiente = NULL;
     free(elemento);
+  }
 }
 
-void Encolar(Cola *cola, u32 v, u32 i){
-    ElementoC *elemento = CrearElementoC(v,i);
-    if(!cola->primer){
-        cola->primer = elemento;
-        cola->ultimo = elemento;
-    }
-    else{
-        cola->ultimo->siguiente = elemento;
-        cola->ultimo = elemento;
-    }
-    cola->longitud++;
+void Encolar(Cola *cola, u32 v) {
+  assert(cola != NULL);
+  ElementoC *elemento = CrearElementoC(v);
+  if(!cola->primer) {
+    cola->primer = elemento;
+    cola->ultimo = elemento;
+  } else {
+    cola->ultimo->siguiente = elemento;
+    cola->ultimo = elemento;
+  }
+  cola->longitud++;
 }
 
-void Eliminar(Cola *cola){
-    if(cola->primer){
-        ElementoC *eliminado = cola->primer;
-        cola->primer = cola->primer->siguiente;
-        DestruirElementoC(eliminado);
-        cola->longitud--;
+void Desencolar(Cola *cola) {
+  assert(cola != NULL);
+  if(cola->primer) {
+    ElementoC *eliminado = cola->primer;
+    cola->primer = cola->primer->siguiente;
+    DestruirElementoC(eliminado);
+    cola->longitud--;
 
-        if(!cola->primer){
-            cola->ultimo = NULL;
-        }
-    }
+    if(!cola->primer)
+        cola->ultimo = NULL;
+  }
 }
 
-Cola *CrearCola(){
-    Cola* cola = (Cola *) malloc(sizeof(Cola));
-    cola->primer = NULL;
-    cola->ultimo = NULL;
-    cola->longitud = 0;
-    return cola;
+Cola *CrearCola() {
+  Cola* cola = (Cola *) malloc(sizeof(Cola));
+  cola->primer = NULL;
+  cola->ultimo = NULL;
+  cola->longitud = 0;
+
+  return cola;
 }
 
-void DestruirCola(Cola *cola){
-    while(cola->primer){
-        Eliminar(cola);
-    }
+void DestruirCola(Cola *cola) {
+  if(cola != NULL) {
+    while(cola->primer)
+      Desencolar(cola);
+
     free(cola);
+  }
 }
 
-u32 PrimerElemento(Cola *cola){
-    if(cola->primer){
-        return cola->primer->vertice;
-    }
-    else{
-        return 0;
-    }
+u32 PrimerElemento(Cola *cola) {
+  assert(cola != NULL);
+  if(cola->primer)
+    return cola->primer->vertice;
+
+  return 0;
 }
 
-u32 IndiceDelPrimero(Cola *cola){
-    if(cola->primer){
-        return cola->primer->indice;
-    }
-    else{
-        return 0;
-    }
+u32 LongitudC(Cola *cola) {
+  assert(cola != NULL);
+  return cola->longitud;
 }
 
-u32 LongitudC(Cola* cola){
-    return cola->longitud;
-}
-
-int EstaVaciaC(Cola* cola){
-    return cola->longitud == 0;
+int EstaVaciaC(Cola *cola) {
+  assert(cola != NULL);
+  return cola->longitud == 0;
 }
--- MARINI_Santiago__CENTINI_Joaquin/CangrejoEstelar/cola.h
+++ MARINI_Santiago__CENTINI_Joaquin/CangrejoEstelar/cola.h
@@ -1,25 +1,28 @@
-typedef struct elementoc{
+#ifndef _COLA_H
+#define _COLA_H
+
+typedef struct elementoc {
     u32 vertice;
-    u32 indice;
     struct elementoc *siguiente;
 } ElementoC;
 
-typedef struct cola{
+typedef struct cola {
     ElementoC *primer;
     ElementoC *ultimo;
     u32 longitud;
 } Cola;
 
-
-
-ElementoC *CrearElementoC(u32 v,u32 i);
-
+// funciones sobre cola 
+ElementoC *CrearElementoC(u32 v);
 void DestruirElementoC(ElementoC *elemento);
-void Encolar(Cola *cola, u32 v, u32 i);
-void Eliminar(Cola *cola);
+void Encolar(Cola *cola, u32 v);
+void Desencolar(Cola *cola);
 Cola *CrearCola();
 void DestruirCola(Cola *cola);
 u32 PrimerElemento(Cola *cola);
 u32 IndiceDelPrimero(Cola *cola);
 u32 LongitudC(Cola* cola);
 int EstaVaciaC(Cola* cola);
+
+
+#endif
